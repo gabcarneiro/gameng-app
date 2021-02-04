6e6629b2971d036a2f1c0f9795c70343125e0466
Gabriel Carneiro <gabcarneiro@outlook.com>
2021-02-03 23:38:58 -0300

    feat: sliver app bar image (wip)

--- lib/modules/game_details/game_details_page.dart
+++ lib/modules/game_details/game_details_page.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 
 class GameDetailsPage extends StatelessWidget {
@@ -5,6 +6,23 @@ class GameDetailsPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Container());
+    return Scaffold(
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            expandedHeight: 200,
+            backgroundColor: Colors.white,
+            flexibleSpace: CachedNetworkImage(
+              fit: BoxFit.cover,
+              imageUrl:
+                  'https://nexushub.co.za/images/gallery/00010/10546_8-_thunderjaws_are_scary.jpg',
+            ),
+          ),
+          SliverToBoxAdapter(
+            child: Center(child: Text('Teste')),
+          ),
+        ],
+      ),
+    );
   }
 }
