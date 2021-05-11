31bab128fb6f580a313902d122059fb2ec2190a1
Gabriel Carneiro <gabcarneiro@outlook.com>
2021-05-11 20:13:03 -0300

    refactor: wraps MaterialApp into AppWidget

--- lib/main.dart
+++ lib/main.dart
@@ -1,33 +1,9 @@
 import 'package:flutter/material.dart';
 
-import 'modules/game_details/game_details_page.dart';
+import 'app_widget.dart';
 
 void main() {
   runApp(
-    MaterialApp(
-      theme: ThemeData(
-        fontFamily: 'Quicksand',
-        textTheme: TextTheme(
-          subtitle1: TextStyle(
-            fontWeight: FontWeight.w900,
-            color: Color(0xFF162B46),
-          ),
-          subtitle2: TextStyle(
-            color: Color(0xFF162B46),
-            fontWeight: FontWeight.w900,
-          ),
-          bodyText1: TextStyle(
-            fontWeight: FontWeight.bold,
-            color: Color(0xFF8994a6),
-          ),
-          bodyText2: TextStyle(
-            fontWeight: FontWeight.bold,
-            color: Color(0xFF8994a6),
-          ),
-        ),
-        primaryColor: Color(0xFF6A5CD5),
-      ),
-      home: GameDetailsPage(),
-    ),
+    AppWidget(),
   );
 }
